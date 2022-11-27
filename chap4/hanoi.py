634ca444c6182cc543c2944c8a717f8da2cc75a3
Kamiichi <paperone430@gmail.com>
2022-11-27 18:18:48 +0900

    深さ優先でディレクトリのファイルを探索する

--- chap4/hanoi.py
+++ chap4/hanoi.py
@@ -6,5 +6,9 @@ def hanoi(n, src, dist, via): #src=移動元, dist=移動先, via=経由場所
     else:
         print(src + ' -> ' + dist)
 
+# インプットのnによる移動回数の参考
+# n=10, 1,023回
+# n=24, 16,777,215回
+# n=64, 1845京
 n = int(input())
 hanoi(n, 'a', 'b', 'c')
