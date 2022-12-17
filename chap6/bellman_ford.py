469b455ae17f0ec39377303b5e3348b459709317
Kamiichi <paperone430@gmail.com>
2022-12-18 02:22:46 +0900

    ダイクストラ法の実装

--- chap6/bellman_ford.py
+++ chap6/bellman_ford.py
@@ -4,6 +4,7 @@ def bellman_ford(edges, num_v):
 
     changed = True
     while changed:  # コストが更新されている間繰り返す
+        print(dist)
         changed = False
         for edge in edges:  # 各辺について繰り返し
             if dist[edge[1]] > dist[edge[0]] + edge[2]:
@@ -15,8 +16,8 @@ def bellman_ford(edges, num_v):
 # 辺のリスト(起点、終点、コストのリスト)
 edges = [
     [0, 1, 4], [0, 2, 3], [1, 2, 1], [1, 3, 1],
-    [1, 4, 5], [2, 5, 2], [4, 6, 2], [5, 4, 1],
-    [5, 6, 4]
+    [1, 4, 5], [2, 5, 2], [3, 4, 3], [4, 6, 2], 
+    [5, 4, 1], [5, 6, 4]
 ]
 
 print(bellman_ford(edges, 7))
